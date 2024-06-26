86310f9b68aa2451c6a75888aef21aafcf5d5300
SML369 <santimejia14@hotmail.com>
2024-06-26 11:43:39 -0500

    version 1

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 import pandas as pd
-        import plotly.express as px
-        import streamlit as st
+import plotly.express as px
+import streamlit as st
+
+st.header('Gráficos de venta de vehiculos en US')
         
-        car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-        hist_button = st.button('Construir histograma') # crear un botón
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
+scatter_button = st.button('Construir gráfico de dispersión')
+
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    fig = px.scatter(car_data, x='odometer', y='price')
+
+    st.plotly_chart(fig, use_container_width= True)
+    
+if hist_button: # al hacer clic en el botón
         
-        if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
-            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')# escribir un mensaje
             
-            # crear un histograma
-            fig = px.histogram(car_data, x="odometer")
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
         
-            # mostrar un gráfico Plotly interactivo
-            st.plotly_chart(fig, use_container_width=True)
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
